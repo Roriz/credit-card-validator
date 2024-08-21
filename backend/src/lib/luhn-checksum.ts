62e3aff6e1bfa43dc0d240e1f18a5634b75feecb
Radamés Roriz <radamesroriz@gmail.com>
2024-08-21 11:34:06 -0300

    fix: adjust luhn checksum

--- backend/src/lib/luhn-checksum.ts
+++ backend/src/lib/luhn-checksum.ts
@@ -1,5 +1,5 @@
 function cardNumbersToDigits(cardNumber: string): number[] {
-  return cardNumber.split('').reverse().map((char: string): number => {
+  return cardNumber.split('').map((char: string): number => {
     const digit: number = parseInt(char);
     if (isNaN(digit)) {
       throw new Error(`Invalid character '${char}' in card number`);
@@ -10,13 +10,19 @@ function cardNumbersToDigits(cardNumber: string): number[] {
 }
 
 function sumEverySecondDigit(digits: number[]): number {
-  return digits.reduce((accumulator: number, digit: number, index: number): number => {
-    return index % 2 === 1 ? accumulator + digit : accumulator;
+  const parity = digits.length % 2;
+
+  return digits.reduce((acc, digit, index) => {
+    if (index % 2 === parity) { digit *= 2; }
+    if (digit > 9) { digit -= 9; }
+
+    return acc + digit;
   }, 0);
 }
 
 export function luhnChecksum(cardNumber: string): boolean {
   const digits: number[] = cardNumbersToDigits(cardNumber);
   const sum: number = sumEverySecondDigit(digits);
+
   return sum % 10 === 0;
 }
