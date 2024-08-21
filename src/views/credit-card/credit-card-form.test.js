cba8335f3c1889d77228195edae9713aab880776
Radamés Roriz <radamesroriz@gmail.com>
2024-08-21 09:55:57 -0300

    ref: cleanup the boostrap code and use project name

--- src/views/credit-card/credit-card-form.test.js
+++ src/views/credit-card/credit-card-form.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from './credit-card-form';
 
 test('renders learn react link', () => {
   render(<App />);
