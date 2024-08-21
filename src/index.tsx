f98d380eccd36b924838ed05dec9e3144a2beaa3
Radamés Roriz <radamesroriz@gmail.com>
2024-08-21 10:08:32 -0300

    feat: add typescript

---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import CreditCardForm from './views/credit-card/credit-card-form.tsx';
+
+const root = ReactDOM.createRoot(document.body);
+
+root.render(<CreditCardForm />);
--- src/views/credit-card/credit-card-form.tsx
+++ src/views/credit-card/credit-card-form.tsx
@@ -1,11 +1,11 @@
 import './credit-card-form.css';
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
